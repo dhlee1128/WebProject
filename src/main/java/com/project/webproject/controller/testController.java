6b88144c820971455fb8fe76ba81c37a0d92cfca
duhui1004@naver.com <duhui1004@naver.com>
2021-11-28 11:45:18 +0900

    react 설치

--- src/main/java/com/project/webproject/controller/testController.java
+++ src/main/java/com/project/webproject/controller/testController.java
@@ -1,4 +1,4 @@
-package com.project.webproject;
+package com.project.webproject.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
